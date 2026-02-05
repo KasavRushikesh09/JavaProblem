0a0feba458e94afcdae372ca4bc007db74bf89eb
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-05 16:15:41 +0530

    Reverse Array

---
+++ Array/ReverseArray.java
@@ -0,0 +1,18 @@
+public class ReverseArray{
+    public static void main(String args[]){
+        int arr[] = {10,50,39,49,60};
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for(int num : arr){
+            System.out.print(num+",");
+        }
+    }
+}
