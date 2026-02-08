90479ea31e0420de7c31fd54f00c444e66bab1e0
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-08 12:12:07 +0530

    Middle Element in list

---
+++ LinkedList/MiddleElement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class MiddleElement {
+    public static void main(String args[]){
+        LinkedList<Integer>list = new LinkedList<>(
+                Arrays.asList(1,2,3,4,5,6,7)
+        );
+
+        System.out.println(list);
+        MiddleElement(list);
+    }
+
+    static void MiddleElement(LinkedList<Integer>list){
+        int slow = 0;
+        int fast = 0;
+
+        while(fast<list.size() && fast+1<list.size()){
+            slow++;
+            fast+=2;
+
+        }
+        System.out.println("middle element is : "+list.get(slow));
+    }
+}
