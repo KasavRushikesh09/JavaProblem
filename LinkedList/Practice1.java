0e5361f3262da9e8dd1767039a5286282b1cb766
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-08 11:04:02 +0530

    Singlular linkedlist based simple problem

---
+++ LinkedList/Practice1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Practice{
+    public static void main(String args[]){
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(10);
+        list.add(30);
+        list.addFirst(5);
+        list.addLast(50);
+        System.out.println(list);
+    }
+}
