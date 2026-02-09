0a01189d0bba5ef5417761a53421ab04e2425fb3
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-09 16:22:31 +0530

    Palindrome problem

---
+++ String/Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class palindrome{
+    public static  void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String s = sc.nextLine();
+        String rev = "";
+
+        for(int i=rev.length()-1;i>=0;i--){
+            rev = rev+s.charAt(i);
+        }
+
+        if(s.equals(rev)){
+            System.out.println("String is Palindrome");
+        }
+        else{
+            System.out.println("String is not Palindrome");
+        }
+    }
+}
