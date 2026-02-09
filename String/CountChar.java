82f2b8e2643bc7f4b884b0152fd0f5c511c51d29
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-09 16:26:01 +0530

    Count charector in string

---
+++ String/CountChar.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CountChar{
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String s = sc.nextLine();
+
+        System.out.println("Count of charector is: "+ s.length());
+    }
+}
