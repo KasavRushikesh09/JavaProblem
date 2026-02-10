03dc8601915fb101f66baca19f3b924d8b3f3b7f
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-10 19:35:13 +0530

    String combinations as per number

--- Array/SumOfAll.java
+++ Array/SumOfAll.java
@@ -1,25 +1,21 @@
+import java.util.Scanner;
 import java.util.*;
-public class sumofall{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array: ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Enter Numbers for Arrays: ");
+class Main {
+    static void SumArray(int array[],int n){
+        int sum = 0;
         for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
+            sum+=array[i];
         }
-        System.out.print("your Array is :");
-        for (int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
         }
-
-        int res = 0;
-        for (int i=0;i<n;i++){
-            res +=arr[i];
-        }
-        System.out.println("Total Sum is : "+res);
-
-
+        // Your code here
+        SumArray(array,n);
     }
 }
---
+++ HighLogicalProblem/StringCombination.java
@@ -0,0 +1,22 @@
+import java.util.*;;
+public class StringCombination{
+    static void combinations(String s,int k, int index, String current){
+        if(current.length() == k){
+            System.out.println(current+" ");
+            return;
+        }
+
+        for(int i=index;i<s.length();i++){
+            combinations(s,k,i+1,current+s.charAt(i));
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int k = sc.nextInt();
+
+        combinations(s,k,0,"");
+
+    }
+}
