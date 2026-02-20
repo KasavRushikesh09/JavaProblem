4e150859a26264352c60c1545758cb2c74b7c97b
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-20 11:10:54 +0530

    Moves zeros at last

---
+++ Array/PlusOne.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class PlusOne{
+
+    public static void main(String args[]){
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<9){
+                return 1;
+            }
+            else if(arr[i] == 9){
+                arr
+            }
+
+        }
+
+    }
+}
