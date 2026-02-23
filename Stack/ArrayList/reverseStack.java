fb2ecc7899f69949cc2183dc3f1b1d26f6e973ba
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2026-02-23 11:47:08 +0530

    reverse Stack

--- Stack/ArrayList/reverseStack.java
+++ Stack/ArrayList/reverseStack.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class reverseStack{
-
+    
     static void reverse(Stack<Integer>s){
         if(s.isEmpty()){
             return;
@@ -17,14 +17,13 @@ public class reverseStack{
         int temp = s.pop();
         pushAtBottom(s,data);
         s.push(temp);
-
     }
     public static void main(String args[]){
         Stack<Integer>s = new Stack<>();
+
         s.push(1);
         s.push(2);
         s.push(3);
-
 
         while(!s.isEmpty()){
             System.out.println(s.pop());
